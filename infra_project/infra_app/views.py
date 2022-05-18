fa6e11434838c675c1f848f042d0f87a754b2ca1
Юрий Новиков <iurij.novickov2016@yandex.ru>
2022-05-18 22:21:16 +0300

    deface 4

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!!!!!!!!!\n\n\n\nДолжно быть сообщение в телеге.')
+    return HttpResponse('У меня получилось!\nДолжно быть сообщение в телеге.')
 
 
 def second_page(request):
